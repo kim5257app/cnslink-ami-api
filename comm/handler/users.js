6500a03f8c5256ffff2936afd295c3a0c60b08f3
kim5257 <kim5257@naver.com>
2022-02-17 01:57:52 +0900

    기능추가중...

--- comm/handler/users.js
+++ comm/handler/users.js
@@ -2,6 +2,7 @@ const db = require('../../db/index');
 const usersQuery = require('../../db/query/users');
 const { onHandler } = require('../helper');
 const { verifyIdToken } = require('../../firebase/auth');
+const Error = require('../../debug/error');
 
 exports.rules = {
   'users.verify': {
@@ -9,6 +10,13 @@ exports.rules = {
   },
   'users.name.update': {
     name: { type: 'string' },
+  },
+  'users.permission.update': {
+    id: { type: 'string', maxLen: 64 },
+    manager: { type: 'boolean', required: false },
+    lookup: { type: 'boolean', required: false },
+  },
+  'users.list.get': {
   },
 };
 
@@ -22,7 +30,10 @@ exports.initHandler = (io, socket) => {
     if (userInfo == null) {
       // 사용자 신규 생성
       await db.getInstance()
-        .query(usersQuery.addUserInfo({ id: decodedIdToken.uid }));
+        .query(usersQuery.addUserInfo({
+          id: decodedIdToken.uid,
+          email: decodedIdToken.email,
+        }));
 
       // 정보 다시 가져오기
       userInfo = await db.getInstance()
@@ -40,6 +51,49 @@ exports.initHandler = (io, socket) => {
       result: 'success',
       userInfo,
       decoded: decodedIdToken,
+    });
+  });
+
+  onHandler(socket, 'users.permission.update', async (payload, resp) => {
+    const { userInfo } = socket.data;
+
+    if (userInfo == null || !userInfo.admin) {
+      Error.throwFail('ACCESS_DENIED', 'Access Denied');
+    }
+
+    // 대상 권한 정보 가져오기
+    const targetOriginInfo = await db.getInstance()
+      .query(usersQuery.getUserInfo({ id: payload.id }));
+
+    await db.getInstance()
+      .query(usersQuery.updatePermission({
+        ...targetOriginInfo,
+        ...payload,
+      }));
+
+    // 업데이트 후 다시 권한 정보 가져오기
+    const targetUpdatedInfo = await db.getInstance()
+      .query(usersQuery.getUserInfo({ id: payload.id }));
+
+    resp({
+      result: 'success',
+      item: targetUpdatedInfo,
+    });
+  });
+
+  onHandler(socket, 'users.list.get', async (payload, resp) => {
+    const { userInfo } = socket.data;
+
+    if (userInfo == null || !userInfo.admin) {
+      Error.throwFail('ACCESS_DENIED', 'Access Denied');
+    }
+
+    const items = await db.getInstance()
+      .query(usersQuery.getUserList());
+
+    resp({
+      result: 'success',
+      items,
     });
   });
 };
--- db/query/users.js
+++ db/query/users.js
@@ -4,14 +4,16 @@
 
 module.exports = {
   addUserInfo: (args) => ({
-    sql: "INSERT INTO users(id) VALUES (:id)",
+    sql: "INSERT INTO users(id, email) VALUES (:id, :email)",
     args,
   }),
   getUserInfo: (args) => ({
     sql:
       "SELECT\n" +
-      "  users.id, users.`name`,\n" +
-      "  user_permission.admin, user_permission.manager\n" +
+      "  users.id, users.`name`, users.email,\n" +
+      "  user_permission.admin,\n" +
+      "  user_permission.manager,\n" +
+      "  user_permission.lookup\n" +
       "FROM users\n" +
       "JOIN user_permission ON user_permission.id=users.id\n" +
       "WHERE users.id=:id",
@@ -20,6 +22,7 @@ module.exports = {
       ...result[0],
       admin: (result[0].admin > 0),
       manager: (result[0].manager > 0),
+      lookup: (result[0].lookup > 0),
     } : null),
   }),
   updateUserName: (args) => ({
@@ -27,6 +30,26 @@ module.exports = {
       "UPDATE users\n" +
       "SET `name`=:name\n" +
       "WHERE id=:id",
+    args,
+  }),
+  updatePermission: (args) => ({
+    sql:
+      "UPDATE user_permission\n" +
+      "SET manager=:manager, lookup=:lookup\n" +
+      "WHERE id=:id",
+    args: {
+      id: args.id,
+      admin: (args.admin ? 1 : 0),
+      manager: (args.manager ? 1 : 0),
+      lookup: (args.lookup ? 1 : 0),
+    },
+  }),
+  getUserList: (args) => ({
+    sql:
+      "SELECT users.id, email, `name`, admin, manager, lookup\n" +
+      "FROM users\n" +
+      "JOIN user_permission ON users.id=user_permission.id\n" +
+      "WHERE admin=0",
     args,
   }),
 };
